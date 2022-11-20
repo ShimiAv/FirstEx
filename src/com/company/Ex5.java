20674331b946fc0528243c7edd9b76c4541c4952
Shimon <shimondj@gmail.com>
2022-11-20 14:52:49 +0200

    FirstEx

--- src/com/company/Ex5.java
+++ src/com/company/Ex5.java
@@ -10,15 +10,13 @@ public class Ex5 {
         int x1 = 0;
         int x2 = 1;
         int x3 = 0;
-        while (x3 < userInput) {
-            x3 = x1 + x2;
-            x1 = x2;
-            x2 = x3;
-        }
-        if (x3 == userInput) {
-            System.out.println("The number you entered is in fibonacci series");
-        } else {
-            System.out.println("The number you entered is not in fibonacci series");
-        }
-    }
-}
+     while (x3<userInput){
+      x3=x1+x2;
+      x1=x2;
+      x2=x3;
+     }
+     if (x3==userInput){
+         System.out.println("The number you entered is in fibonacci series");
+     }else {
+         System.out.println("The number you entered is not in fibonacci series");
+    }}}
